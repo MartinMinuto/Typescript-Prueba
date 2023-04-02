0a54cd101d7ea511d3942368ec44a23ec26e2959
MartinMinuto <martin.minuto1@gmail.com>
2023-04-02 17:37:12 -0300

    Config2.2

--- Typescript/dist/index.js
+++ Typescript/dist/index.js
@@ -1,8 +1,5 @@
 "use strict";
-function suma(a, b) {
-    return a + b;
-}
-console.log(suma(5, 8));
+Object.defineProperty(exports, "__esModule", { value: true });
 let Saludar = 'Hello wordl';
 let Names = ['Carlos', 'Felipe', 'Gaston'];
 var Constract;
@@ -15,4 +12,53 @@ const ConstractStatus = Constract.permanet;
 console.log(ConstractStatus);
 console.log(Saludar + ' ' + Names[2]);
 console.log(Names);
+function SumNumber(num1, num2) {
+    return (num1 + num2);
+}
+SumNumber(2, 2);
+function buildArray(items, sortOrder) {
+    let randomNumbers = [];
+    let nextNumber;
+    for (let counter = 0; counter < items; counter++) {
+        nextNumber = Math.ceil(Math.random() * (100 - 1));
+        if (randomNumbers.indexOf(nextNumber) === -1) {
+            randomNumbers.push(nextNumber);
+        }
+        else {
+            counter--;
+        }
+    }
+    if (sortOrder === 'ascending') {
+        return randomNumbers.sort(sortAscending);
+    }
+    else {
+        return randomNumbers.sort(sortDescending);
+    }
+}
+let sortDescending = (a, b) => {
+    if (a > b) {
+        return -1;
+    }
+    else if (b > a) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+};
+let sortAscending = (a, b) => {
+    if (a > b) {
+        return 1;
+    }
+    else if (b > a) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+};
+let myArray1 = buildArray(12, 'ascending');
+let myArray2 = buildArray(8, 'descending');
+console.log(myArray1);
+console.log(myArray2);
 //# sourceMappingURL=index.js.map
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,10 +1,5 @@
 import { string } from "yargs"
 
-function suma(a: number,b: number) {
-    return a + b 
-}
-
-console.log(suma(5,8))
 
 let Saludar: string = 'Hello wordl'
 let Names: string[] = ['Carlos', 'Felipe','Gaston']
@@ -21,3 +16,55 @@ console.log(ConstractStatus)
 
 console.log(Saludar + ' ' + Names[2])
 console.log(Names)
+
+
+function SumNumber(num1:number, num2: number): number {
+    return(num1 + num2)
+}
+
+SumNumber(2,2)
+
+function buildArray(items: number, sortOrder: 'ascending' | 'descending'): number[] {
+    let randomNumbers: number[] = [];
+    let nextNumber: number;
+ 
+    for (let counter = 0; counter < items; counter++) {
+        nextNumber = Math.ceil(Math.random() * (100 - 1));
+        if (randomNumbers.indexOf(nextNumber) === -1) {
+          randomNumbers.push(nextNumber);
+        } else {
+          counter--;
+        }
+    }
+ 
+    if (sortOrder === 'ascending') {
+      return randomNumbers.sort(sortAscending);
+    } else {
+      return randomNumbers.sort(sortDescending);
+    }
+ }
+
+ let sortDescending = (a:number, b:number) => {
+    if (a > b) {
+        return -1;
+    } else if (b > a) {
+        return 1;
+    } else {
+        return 0;
+    }
+ }
+ 
+ let sortAscending = (a:number, b:number) => {
+     if (a > b) {
+       return 1;
+     } else if (b > a) {
+       return -1;
+     } else {
+       return 0;
+     }
+   }
+
+   let myArray1 = buildArray(12, 'ascending');
+    let myArray2 = buildArray(8, 'descending');
+    console.log(myArray1);
+    console.log(myArray2);
